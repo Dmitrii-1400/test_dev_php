b2955f820fb445b8eea86033a9f2b5626ebe4510
Dmitry <72274326+Dmitrii-1400@users.noreply.github.com>
2024-01-26 10:17:54 +0700

    Create AuthController.php

---
+++ AuthController.php
@@ -0,0 +1,17 @@
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Логика регистрации пользователя
+    }
+
+    public function login(Request $request)
+    {
+        // Логика авторизации пользователя
+    }
+}
